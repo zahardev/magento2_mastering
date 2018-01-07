129c99723256fa6678d8ff68ef7c8f7d91449d77
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-01-07 21:23:57 +0200

    Controllers realized

---
+++ app/code/Mastering/SampleModule/Controller/Index/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mastering\SampleModule\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action {
+	public function execute(){
+		/** @var Raw $result */
+		$result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+		$result->setContents('Hello World!');
+		return $result;
+	}
+}
