975ccf3e1c59995a6e1114c0fb3a780ba54f8403
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-01-08 23:29:11 +0200

    Admin settings for crontab

--- app/code/Mastering/SampleModule/Console/Command/AddItem.php
+++ app/code/Mastering/SampleModule/Console/Command/AddItem.php
@@ -3,6 +3,7 @@
 namespace Mastering\SampleModule\Console\Command;
 
 use Magento\Framework\Console\Cli;
+use Mastering\SampleModule\Model\Config;
 use Symfony\Component\Console\Command\Command;
 use Mastering\SampleModule\Model\ItemFactory;
 use Mastering\SampleModule\Model\ResourceModel\Item as ResourceItem;
@@ -17,11 +18,13 @@ class AddItem extends Command
 
     private $itemFactory;
     private $resourceItem;
+    private $config;
 
-    public function __construct(ItemFactory $itemFactory, ResourceItem $resourceItem)
+    public function __construct(ItemFactory $itemFactory, ResourceItem $resourceItem, Config $config)
     {
         $this->itemFactory = $itemFactory;
         $this->resourceItem = $resourceItem;
+        $this->config = $config;
         parent::__construct();
     }
 
@@ -42,17 +45,19 @@ class AddItem extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $item = $this->itemFactory->create(); /** @var $item \Mastering\SampleModule\Model\Item */
+        if($this->config->isEnabled()){
+            $item = $this->itemFactory->create(); /** @var $item \Mastering\SampleModule\Model\Item */
 
-        $item->setName($input->getArgument(self::INPUT_KEY_NAME));
-        $item->setDescription($input->getArgument(self::INPUT_KEY_DESCRIPTION));
+            $item->setName($input->getArgument(self::INPUT_KEY_NAME));
+            $item->setDescription($input->getArgument(self::INPUT_KEY_DESCRIPTION));
 
-        try{
-            $this->resourceItem->save($item);
-        } catch (\Exception $e) {
-            return Cli::RETURN_FAILURE;
+            try{
+                $this->resourceItem->save($item);
+            } catch (\Exception $e) {
+                return Cli::RETURN_FAILURE;
+            }
+
+            return Cli::RETURN_SUCCESS;
         }
-
-        return Cli::RETURN_SUCCESS;
     }
 }
