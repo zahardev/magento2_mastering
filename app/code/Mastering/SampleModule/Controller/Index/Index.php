0351f529c579f96d6537ce21dc5f0acec3b053f1
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-01-08 16:23:57 +0200

    Layout and view files

--- app/code/Mastering/SampleModule/Controller/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Index/Index.php
@@ -3,14 +3,10 @@
 namespace Mastering\SampleModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
-use Magento\Framework\Controller\Result\Raw;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action {
 	public function execute(){
-		/** @var Raw $result */
-		$result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-		$result->setContents('Hello World!');
-		return $result;
+		return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
 	}
 }
